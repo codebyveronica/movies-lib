360afd15e3b729b7871440be71bc46102671f47f
Veronica <codebyveronica@gmail.com>
2024-12-12 11:39:55 -0300

    add movie card component
    I added and created the component that will be responsible for storing the card structure that will be used for all movies and I created the logic to render the movies collected from the API

--- movieslib/src/main.jsx
+++ movieslib/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="/" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
