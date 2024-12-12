4ac288c3fd2af61a874e2a03a98de1966a459aa2
Veronica <codebyveronica@gmail.com>
2024-12-12 11:38:16 -0300

    add basic struture
    I added the necessary pages and components to start the basic structure of the page

--- movieslib/src/App.jsx
+++ movieslib/src/App.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1>Hello World</h1>
-  )
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+export default App;
